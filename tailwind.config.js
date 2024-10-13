76d2f89d937dd880285d652ca596c0d63b612260
Nora Skjevik <nosk0001@stud.kea.dk>
2024-10-13 21:07:29 +0200

    done!

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,23 @@ export default {
       colors: {
         customWhite: "#FEFFF1",
         customBlack: "#09030D",
+        customRed: "#CB0400",
       },
       cursor: {
         handcursor: "url(/cursor.png) 25 25, pointer",
+        click: "url(/click.png) 25 25, pointer",
+      },
+      fontFamily: {
+        display: ["Playfair Display", "serif"],
+      },
+      animation: {
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
       },
     },
   },
